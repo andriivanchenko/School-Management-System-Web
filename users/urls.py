f2c9e6252857c70c3c1483342c33a7b89179b290
andriivanchenko <ivan4enko.andrey01@gmail.com>
2025-01-25 09:23:03 +0200

    fixed logout and removed navbar

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 
 urlpatterns = [
     path("login", LoginView.as_view(), name="login"),
-    path("logout", LogoutView.as_view(), name="logout"),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
