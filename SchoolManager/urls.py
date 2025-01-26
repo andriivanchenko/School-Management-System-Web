b39b9443f4aeeb0b17ac4f15e17380a77126d01d
andriivanchenko <ivan4enko.andrey01@gmail.com>
2025-01-26 16:11:22 +0200

    made navbar and authentication

--- SchoolManager/urls.py
+++ SchoolManager/urls.py
@@ -19,10 +19,12 @@ from django.contrib.auth import get_user_model
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
+from users.views import home_view
+
 User = get_user_model()
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("", home_view, name="home"),
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
     path("", include("django.contrib.auth.urls")),
---
+++ users/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth import authenticate
+from django.core.exceptions import ValidationError
+
+class LoginForm(forms.Form):
+    user_login = forms.CharField(
+        label="Логін",
+        max_length=150,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введіть логін',
+        }),
+    )
+    password = forms.CharField(
+        label="Пароль",
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Введіть пароль',
+        }),
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        user_login = cleaned_data.get('user_login')
+        password = cleaned_data.get('password')
+
+        if user_login and password:
+            user = authenticate(username=user_login, password=password)
+            if not user:
+                raise ValidationError("Неправильний логін або пароль")
+        return cleaned_data
+
