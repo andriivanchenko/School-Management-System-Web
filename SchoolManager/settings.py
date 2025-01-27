1480e6a3a7151779426b84760ad87c0c76bd3c9f
PoliakovAlex <al.ev.poliakov@gmail.com>
2025-01-27 20:29:05 +0200

    fix login page

--- SchoolManager/settings.py
+++ SchoolManager/settings.py
@@ -88,10 +88,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('MYSQL_DATABASE'),
-        'USER': os.getenv('MYSQL_USER'),
-        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-        'HOST': os.getenv('MYSQL_HOST'),
-        'PORT': os.getenv('MYSQL_PORT'),
+        'USER': os.getenv('MYSQL_ROOT_USER'),
+        'PASSWORD': os.getenv('MYSQL_ROOT_PASSWORD'),
+        # 'HOST': os.getenv('MYSQL_HOST'),
+        # 'PORT': os.getenv('MYSQL_PORT'),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
