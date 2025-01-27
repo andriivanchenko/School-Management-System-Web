9c26edcb4e46eefcb23ad736f5d4bbe8ede5110c
andriivanchenko <ivan4enko.andrey01@gmail.com>
2025-01-27 20:30:53 +0200

    сommit of users

--- SchoolManager/settings.py
+++ SchoolManager/settings.py
@@ -90,8 +90,8 @@ DATABASES = {
         'NAME': os.getenv('MYSQL_DATABASE'),
         'USER': os.getenv('MYSQL_ROOT_USER'),
         'PASSWORD': os.getenv('MYSQL_ROOT_PASSWORD'),
-        # 'HOST': os.getenv('MYSQL_HOST'),
-        # 'PORT': os.getenv('MYSQL_PORT'),
+        #'HOST': os.getenv('MYSQL_HOST'),
+        #'PORT': os.getenv('MYSQL_PORT'),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
